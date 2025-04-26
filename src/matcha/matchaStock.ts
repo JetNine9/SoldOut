329a9cf72a86ca9f6d337af0e2d3e6a7aeed3f81
Jethro Talavera <jet@Jethros-MacBook-Air.local>
2025-04-26 10:57:44 -0400

    first-commit

---
+++ src/matcha/matchaStock.ts
@@ -0,0 +1,31 @@
+import { chromium } from 'playwright';
+
+async function checkStock() {
+
+  const browser = await chromium.launch({ headless: false, slowMo: 1000 });
+  const context = await browser.newContext({
+
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+  });
+  const page = await context.newPage();
+
+  await page.goto('https://www.matchajp.net/products/koyamaen-matcha-tea-powder-ceremonical-grade-ogurayama-150g-can',
+     { waitUntil: 'domcontentloaded' });
+
+  try {
+    const button = await page.waitForSelector('#ProductSubmitButton-template--15972096901273__main', { timeout: 5000 });
+    const buttonText = await button.innerText();
+
+    if (buttonText.includes('Sold out')) {
+      console.log('🟥 Koyamaen Matcha is SOLD OUT');
+    } else {
+      console.log('🟩 Koyamaen Matcha Item is IN STOCK!');
+    }
+  } catch (err) {
+    console.error('❌ Could not find the button:', err.message);
+  }
+
+  await browser.close();
+}
+
+checkStock();
